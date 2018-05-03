9d273a30044f4294f246d4cd1a4121e6a98d8302
daleym <daleym>
2018-05-03 23:12:08 +0800

    add

--- src/com/ym/collection/TestMap.java
+++ src/com/ym/collection/TestMap.java
@@ -1,16 +1,30 @@
 package com.ym.collection;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
- * 
+ * HashMap遍历方式之 Entry<K,V>
+ * 1.相比取出遍历key，再去集合中取值，效率高！
+ * 2.元素无序！
+ * 3.线程不安全
+ * 4.可以放入null键值
  * @author Administrator
  *
  */
 public class TestMap {
 
 	public static void main(String[] args) {
-	}
+		
+		Map<String,String> maps = new HashMap<>();
+		maps.put("second", "dale");
+		maps.put("first", "lily");
+		maps.put(null, null);
+		Set<Entry<String ,String>> entrySet = maps.entrySet();
+		for(Entry< String , String > e : entrySet){
+			System.out.println(String.format("key【%s】:value【%s】", e.getKey() , e.getValue()));
+		}
+ 	}
 }
