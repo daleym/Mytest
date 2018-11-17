90ad44745cc6cfab24488cc51446893ab04d643b
daleym <daleym>
2018-11-17 22:51:40 +0800

    add

--- src/com/ym/dataStructuresAndAlgorithms/binarySearch/TestSquareRootByBinarySearch.java
+++ src/com/ym/dataStructuresAndAlgorithms/binarySearch/TestSquareRootByBinarySearch.java
@@ -1,37 +1,18 @@
 package com.ym.dataStructuresAndAlgorithms.binarySearch;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-
 /**
  * @description 二分查找算法：			          
  * 应用场景：开平方根（sqrt）的实现
  * 要求：1.保留小数点后六位 
- * 	   2.误差精度在0.00001以内
+ * 	   2.误差精度在0.00001以内(六位)
  * eg:16.0的平方根是：4.000000636692939
- * @author ming.yang
+ *    49.0的平方根是：7.000000141269659
+ *    51.0的平方根是：7.141428637096297
  */
 public class TestSquareRootByBinarySearch {
-
-	public static void main(String[] args) {
-		for (double i = 1; i <= 100; i++) {
-			Double a = binSearchSqrt(i, 1d, i);
-			System.out.println(i + "的平方根是：" + a);
-			System.out.println(String.format("保留6位小数(向下取整)：【%s】", new BigDecimal(a).setScale(6 , RoundingMode.DOWN).doubleValue() ));
-		}
-		double tmp = 2D/1.5D;
-		System.out.println(tmp);
-		
-	}
-
 	private static double binSearchSqrt(Double num, Double left, Double right) {
-		if(2 == num){
-			System.out.println("debug start.......");
-		}
-		if (num == null) {
-			throw new IllegalArgumentException("input invalid");
-		}
-
+		if (num == null) throw new IllegalArgumentException("input invalid");
 		while (left < right) {
 			double middle = (left + right) / 2.0;
 			if (Math.abs(middle * middle - num) < 0.00001) {
@@ -48,4 +29,12 @@ public class TestSquareRootByBinarySearch {
 		return left;
 	}
 
+	public static void main(String[] args) {
+		for (double i = 1; i <= 100; i++) {
+			Double a = binSearchSqrt(i, 1d, i);
+			System.out.println(i + "的平方根是：" + a);
+			System.out.println(String.format("保留6位小数(向下取整)：【%s】", new BigDecimal(a).setScale(6 , RoundingMode.DOWN).doubleValue() ));
+		}
+		
+	}
 }
